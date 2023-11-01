26454060a69db66459a3f384f6c60b0ae2414e05
Guillaume Rahbari <guillaume.rahbari@protonmail.com>
2023-11-01 09:18:52 +0100

    feat: add i18n provider

--- src/providers/UmbrellaProvider.tsx
+++ src/providers/UmbrellaProvider.tsx
@@ -1,19 +1,25 @@
 import { Theme, ThemeProvider, Global, CSSObject } from "@emotion/react";
 import { PropsWithChildren } from "react";
+import { I18nextProvider } from "react-i18next";
+import type { i18n } from "i18next";
 
 interface UmbrellaProps extends PropsWithChildren {
   globalStyle?: CSSObject;
   theme: Theme;
+  i18n: i18n;
 }
 export default function UmbrellaProvider({
   theme,
   globalStyle,
   children,
+  i18n,
 }: UmbrellaProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={globalStyle} />
-      {children}
-    </ThemeProvider>
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyle} />
+        {children}
+      </ThemeProvider>
+    </I18nextProvider>
   );
 }
