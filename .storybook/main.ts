7f445b3c1569084a1f842ea2526d14295fdd9d15
Guillaume Rahbari <guillaume.rahbari@protonmail.com>
2023-11-02 17:49:28 +0100

    chore: fix babel config for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -17,16 +17,16 @@ const config: StorybookConfig = {
     autodocs: "tag",
   },
   async webpackFinal(config) {
+    //@ts-expect-error
+    config.module.rules[2].use[0].options.presets[0][1] = {
+      runtime: "automatic",
+      importSource: "@emotion/react",
+    };
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     //@ts-expect-error
     config.module.rules[2].exclude = [
       /node_modules\/(?!(gatsby|gatsby-script)\/)/,
     ];
-    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-    //@ts-expect-error
-    config.module.rules[2].use[0].options.plugins.push([
-      require.resolve("babel-plugin-remove-graphql-queries"),
-    ]);
     //@ts-expect-error
     config.resolve.mainFields = ["browser", "module", "main"];
     return config;
